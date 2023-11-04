8d24124e34c9b31b455bafbe7d07a4dc2bd068b0
Gabriel-Chaves0 <g.chaves0110@gmail.com>
2023-11-04 16:18:33 -0300

    Agora sim adicionando

---
+++ Game.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int dado();
+void exibirTabuleiro();
+
+int main() {
+    exibirTabuleiro();
+
+    printf("\n Dado: %d", dado());
+
+}
+
+int dado(){
+
+  int numero;
+  srand((unsigned)time(NULL));
+  numero = rand() % 4 + 1;
+  return numero;
+
+}
+
+void exibirTabuleiro(){
+    int i;
+    char teste[] = "Brum"; 
+    printf(" |Inicio| ");
+
+    for (i = 0; i < 10; i++) {
+        printf(" |%d| ", i);
+    }
+    // adicionar para receber struct do lote
+    // printf("\n Você está na casa: %s", Lotes.nome)
+    printf("\n Voce esta no lote: %s", teste);
+}
