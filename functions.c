2373f236599aaa486a7348ebab2e1e4fc775469a
CarlosEdu <carlos.mec2005@gmail.com>
2023-11-09 13:23:33 -0300

    Função de formar e mostrar o placar

--- functions.c
+++ functions.c
@@ -5,6 +5,7 @@
 #include "functions.h"
 
 int dado(){
+    int numero;
 
     printf("Jogando dado");
     for(int i = 0; i < 3; i++){
@@ -12,7 +13,7 @@ int dado(){
         printf(".");
     }
 
-    int numero;
+    
     srand((unsigned)time(NULL));
     numero = rand() % 4 + 1;
     sleep(1);
@@ -44,9 +45,7 @@ void andar_tabuleiro(Lotes *lote, Jogador *jogador){
     Lotes *aux = lote;
     int valor_dado = dado();
 
-
     if(jogador->posicao == 5){ //Verifica re o valor do dado rolado foi o maior / alterar a posição pra a posição da prisão
-    
         if(valor_dado == 4){
             valor_dado = dado(); //Joga o dado depois de sair da prisão
 
@@ -62,7 +61,6 @@ void andar_tabuleiro(Lotes *lote, Jogador *jogador){
     }
 
     for(int i = 0; i < ((jogador->posicao)+valor_dado); i++){ //Ir até a posição do jogador e andar as casas
-
         if(strcmp(aux->nome, "inicio") == 0){ //Mudar o valor a receber quando chegar ao início
             jogador->dinheiro + 20; 
         }
@@ -74,9 +72,45 @@ void andar_tabuleiro(Lotes *lote, Jogador *jogador){
         }
     } 
 
+    //Prinando a casa em que o jogador caiu
     if(strcmp(aux->nome, "Prisao") == 0){
         printf("Você foi preso :(\nPara sair da prisao, role 4 no dado no proximo turno\n");
     }
-    
+    else if(strcmp(aux->nome, "Inicio") == 0){
+        printf("Você está no início, apenas descance :)");
+    }
+    else{
+        printf("Dono: %d\nValor: %d\nAluguel: %d\nNome: %s\n\n", aux->dono, aux->valor, aux->aluguel, aux->nome);
+    }
 }
 
+void remover_jogador(Lotes *lote, Jogador *jogador){
+    Lotes *aux = lote;
+
+    for(int i = 0; i < 15; i++){ //Tirando o jogador como dono dos lotes que ele tem
+        if(aux->dono == jogador->id){
+            aux->dono = 0;
+        } 
+        aux = aux->next;
+    }
+
+    jogador->falido = 1;
+}
+
+void placar(Jogador jogadores[], int n){
+    for(int i = 0; i < n-1; i++){ //Ordenar os jogadores pelo dinheiro que eles tem
+        for (int j = 0; j < n-i-1; j++){
+            if (jogadores[j].dinheiro > jogadores[j+1].dinheiro){
+                int temp = jogadores[j].dinheiro;
+                jogadores[i] = jogadores[j+1];
+                jogadores[j+1].dinheiro = temp;
+            }
+        }
+    }
+
+    printf("|  Colocacao  |  Nome  |  Fortuna  |\n"); //Printar o placar
+    for(int i = 0; i < n; i++){
+        printf("%d %s %d\n", i, jogadores[i].nome, jogadores[i].dinheiro);
+    }
+    
+}
