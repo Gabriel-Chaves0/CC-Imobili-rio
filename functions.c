7362ed79a46dbfe5a3c2485db9735463f0c2804e
CarlosEdu <carlos.mec2005@gmail.com>
2023-11-09 12:06:08 -0300

    Função de andar no tabuleiro feita

--- functions.c
+++ functions.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "functions.h"
 
 int dado(){
 
+    printf("Jogando dado");
+    for(int i = 0; i < 3; i++){
+        sleep(1);
+        printf(".");
+    }
+
     int numero;
     srand((unsigned)time(NULL));
     numero = rand() % 4 + 1;
+    sleep(1);
+
+    printf("\nValor rolado: %d\n", numero);
     return numero;
 
 }
@@ -27,5 +37,46 @@ void exibirTabuleiro(){
 
 void inserir_lotes(Lotes *head, int dono, int valor, int aluguel, char nome ){
     print("inserir funcao");
+}
+
+
+void andar_tabuleiro(Lotes *lote, Jogador *jogador){
+    Lotes *aux = lote;
+    int valor_dado = dado();
+
+
+    if(jogador->posicao == 5){ //Verifica re o valor do dado rolado foi o maior / alterar a posição pra a posição da prisão
+    
+        if(valor_dado == 4){
+            valor_dado = dado(); //Joga o dado depois de sair da prisão
+
+            printf("Jogando dado...\n");
+            sleep(2);
+            printf("Valor rolado: %d\n", valor_dado);
+           
+        }
+        else{
+            printf("Voce nao saiu da prisao :(\n");
+            return;
+        }
+    }
+
+    for(int i = 0; i < ((jogador->posicao)+valor_dado); i++){ //Ir até a posição do jogador e andar as casas
+
+        if(strcmp(aux->nome, "inicio") == 0){ //Mudar o valor a receber quando chegar ao início
+            jogador->dinheiro + 20; 
+        }
+        aux = aux->next;
+
+        jogador->posicao++; //Atualizando a posiçao do jogador
+        if(jogador->posicao == 16){
+            jogador->posicao = 0;
+        }
+    } 
+
+    if(strcmp(aux->nome, "Prisao") == 0){
+        printf("Você foi preso :(\nPara sair da prisao, role 4 no dado no proximo turno\n");
+    }
+    
 }
 
--- functions.h
+++ functions.h
@@ -43,7 +43,7 @@ void exibirTabuleiro(){
 void inserir_lotes(Lotes *head,int dono, int valor, int aluguel, char nome ); // adicionar todos os lotes do jogo
 void comprar_lote(Lotes *head, Jogador *player); // colocar dono no lote atual e tirar dinheiro do player atual
 void pagar_aluguel(Lotes *head, Jogador *inquilino, Jogador *dono); // tirar dinheiro do player atual e colocar dinheiro para o dono
-void andar_tabuleiro(Jogador *head, int posicao_jogador, int dado);// mudar a posição do jogador e verificar em qual casa ele caiu
+void andar_tabuleiro(Lotes *lote, Jogador *jogador);// mudar a posição do jogador e verificar em qual casa ele caiu
 // Dentro da função andar vai pecisar adicionar o salario ao passar pelo lote inicio
 // Se houver funcionalidade da prisão, vai ocorrer dentro de andar tabuleiro
 void placar();// exibir de forma orndenada o placar do jogo por moedas
